357189af47ed1107fd018072ca60284a05c64407
Jemima Ruming <jemima.ruming@gmail.com>
2019-04-09 09:59:04 +0000

    Practice using char

--- main.c
+++ main.c
@@ -1,9 +1,14 @@
 //ENG1003 Project 1
 #include <stdio.h>
 int main() {
-    char str[] = "hello world";
+    char word[20];
+    printf("Enter a word: \n");
+    scanf("%s", word);
+    printf("You entered:  %s\n", word);
+    
+    /*char str[] = "hello world";
     printf("%s \n", str);
     
-   //printf("Hello");
+   //printf("Hello");*/
   return 0; 
 }
