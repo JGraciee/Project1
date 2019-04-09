c2198e6979878ee5f8dc6865f1f48774b10d89a1
Jemima Ruming <jemima.ruming@gmail.com>
2019-04-09 10:46:34 +0000

    First two assessment dot points drafted as switch statements. Working for text input with correct text output. Encryption and Decryption using rotation. Have not texted capitals or white space.

--- main.c
+++ main.c
@@ -1,20 +1,42 @@
 //ENG1003 Project 1
 #include <stdio.h>
 int main() {
-    int i, x;
+    int i, x, k; // k = key 
     char str[100]; // declares a string of 99 characters + null 
     
     
-    printf("\nPlease enter a string: ");
+    printf("\n Enter a string: ");
     scanf("%s", str);
     
+    printf("\n Enter an integer for key: ");
+    scanf("%d", &k);
+    
+    
     printf("\nPlease choose from the following options:\n");
-    printf("1 = Encrypt string.\n");
+    printf("1 = Encrypt string using character rotation.\n");
     printf("2 = Decrypt string.\n");
     scanf("%d", &x);
+    
+    //using switch case statements --> need to make them into functions 
+   switch(x) // refers to option selected above 
+   {
+   case 1:
+      for(i = 0; (i < 100 && str[i] != '\0'); i++) // Encryption is working using rotation 
+        str[i] = str[i] + k; //the key for encryption is 3 that is added to ASCII value
 
+      printf("\nEncrypted string: %s\n", str);
+      break;
 
-//switch case statement for the above options 
+   case 2:
+      for(i = 0; (i < 100 && str[i] != '\0'); i++) // not really sure about this decryption thing :/ 
+        str[i] = str[i] - k; //the key for encryption is 3 that is subtracted to ASCII value
+
+      printf("\nDecrypted string: %s\n", str);
+      break;
+
+   default:
+      printf("\nError\n");
+}
     
     return 0;
     
